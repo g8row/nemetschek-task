dea79327edc7c552a8492a3d08130ec358a0a035
gurow <aliogu23@gmail.com>
2021-06-08 20:54:53 +0300

    small check

--- src/g8row/Main.java
+++ src/g8row/Main.java
@@ -19,7 +19,7 @@ public class Main{
         }catch (IOException error){
             error.printStackTrace();
         }
-        //well, i did it, i did not invent a new sorting method nor implemented it myself, but i do like to work smarter, not harder(i hope that excuses laziness haha)
+        // well, i did it, i did not invent a new sorting method nor implemented it myself, but i do like to work smarter, not harder(i hope that excuses laziness haha)
         // i like to learn new things and technologies, that will help me in the future, so i'm using git, as it's a must for the industry (this github repo contains my super secret project, but i started very recently so it's not much, perhaps it could get me a few additional points haha)
         // also i hope you like jokes and don't sanction me
     }
